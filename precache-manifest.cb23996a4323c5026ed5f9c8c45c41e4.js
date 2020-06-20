ac8735d47c651dcbcbc5df6c9990b8ed717dec8b
tvu20 <tvudinh20@gmail.com>
2020-06-20 09:12:07 -0400

    Updates

---
+++ precache-manifest.cb23996a4323c5026ed5f9c8c45c41e4.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ac26c1d4dfc9dae45b8f5591c1228f",
+    "url": "/mentorshipforgrowth/index.html"
+  },
+  {
+    "revision": "ae25a80fe7f6d3ef89b0",
+    "url": "/mentorshipforgrowth/static/css/main.acb8d032.chunk.css"
+  },
+  {
+    "revision": "d2708a6108492e09d022",
+    "url": "/mentorshipforgrowth/static/js/2.f39ff3de.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mentorshipforgrowth/static/js/2.f39ff3de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae25a80fe7f6d3ef89b0",
+    "url": "/mentorshipforgrowth/static/js/main.75430cc7.chunk.js"
+  },
+  {
+    "revision": "669d67045633e28b20d8",
+    "url": "/mentorshipforgrowth/static/js/runtime-main.cc07b210.js"
+  },
+  {
+    "revision": "0f8bab9872cff3dc65a2ad35a4da58b8",
+    "url": "/mentorshipforgrowth/static/media/audience.0f8bab98.jpg"
+  },
+  {
+    "revision": "2f2aaee6a29c17a82a05ba24dcae2195",
+    "url": "/mentorshipforgrowth/static/media/bridge.2f2aaee6.jpg"
+  },
+  {
+    "revision": "a1bb01ec54fa2151ad97da69f3b37e53",
+    "url": "/mentorshipforgrowth/static/media/career.a1bb01ec.jpg"
+  },
+  {
+    "revision": "0e828bfb91554e3b9114461147f64455",
+    "url": "/mentorshipforgrowth/static/media/chi.0e828bfb.jpg"
+  },
+  {
+    "revision": "d56a13261bf1751766d3b48b0324cde8",
+    "url": "/mentorshipforgrowth/static/media/community.d56a1326.jpg"
+  },
+  {
+    "revision": "fca203da964a6278dfe0d86116ec79c5",
+    "url": "/mentorshipforgrowth/static/media/harvard.fca203da.jpg"
+  },
+  {
+    "revision": "9c3a3e707997b90ada3dc3867a2bd1a1",
+    "url": "/mentorshipforgrowth/static/media/join.9c3a3e70.jpg"
+  },
+  {
+    "revision": "7bdada0c07df126abf2fc35617d063f7",
+    "url": "/mentorshipforgrowth/static/media/leader.7bdada0c.jpg"
+  },
+  {
+    "revision": "a6c7421da50e4fa3f65bcefc6f441e80",
+    "url": "/mentorshipforgrowth/static/media/logo.a6c7421d.png"
+  },
+  {
+    "revision": "1d9ed22e76356b774ed52315b92af3c4",
+    "url": "/mentorshipforgrowth/static/media/mentor.1d9ed22e.jpg"
+  },
+  {
+    "revision": "9d7051ab0e58eedaa6113601b9cb6543",
+    "url": "/mentorshipforgrowth/static/media/mountains.9d7051ab.jpg"
+  },
+  {
+    "revision": "cd2babe81352bc68d1d5743442bbe683",
+    "url": "/mentorshipforgrowth/static/media/network.cd2babe8.jpg"
+  },
+  {
+    "revision": "50ae6f143f0728aaf7606cb0edee1e74",
+    "url": "/mentorshipforgrowth/static/media/offer.50ae6f14.jpg"
+  },
+  {
+    "revision": "db00011d36c92e8fdf9003eefbef9cb1",
+    "url": "/mentorshipforgrowth/static/media/program.db00011d.jpg"
+  },
+  {
+    "revision": "2b5024752a5f945c1066e69bdbe4ede9",
+    "url": "/mentorshipforgrowth/static/media/skill.2b502475.jpg"
+  },
+  {
+    "revision": "68280829b82a72bd9f503c8cd73f8cf7",
+    "url": "/mentorshipforgrowth/static/media/tree.68280829.jpg"
+  },
+  {
+    "revision": "d0a3b306c81855cec3a5222be36b91f4",
+    "url": "/mentorshipforgrowth/static/media/tri.d0a3b306.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aeef35a4d4646b6c6baae2603ce45f87.js"
+  "/mentorshipforgrowth/precache-manifest.cb23996a4323c5026ed5f9c8c45c41e4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mentorshipforgrowth/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
